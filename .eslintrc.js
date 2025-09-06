1861b7fba7a22d7f5a7075ac14b7cdf04039250d
Ding <3206166307@qq.com>
2025-09-06 22:50:24 +0800

    Created the basic structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,36 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es2021: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 添加以下规则解决defineEmits、defineProps等编译器宏的报错
+    'no-undef': 'off',
+    'vue/script-setup-uses-vars': 'error' // 这个规则很重要！
+  },
+  globals: {
+    // 声明Vue 3的编译器宏为全局变量
+    defineEmits: 'readonly',
+    defineProps: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
   }
 }
